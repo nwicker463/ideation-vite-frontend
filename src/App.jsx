baeaa7c5e6ac7cd0bbac1e40463f8764d01b0005
Nate Wicker <wickern7@gmail.com>
2025-07-10 11:50:42 -0400

    updating to show link between pages”

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { Input } from '@/components/ui/input';
 import { Textarea } from '@/components/ui/textarea';
 import { Label } from '@/components/ui/label';
 import './App.css';
+import { Link } from 'react-router-dom';
 
 
 const conjunctivePhrases = [
