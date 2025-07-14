2342edf932a266549c829798a33682b4d3a6bc46
Nate Wicker <wickern7@gmail.com>
2025-07-14 13:08:19 -0400

    making login page first

--- src/main.jsx
+++ src/main.jsx
@@ -21,6 +21,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
         <Route path="/app" element={<ProtectedApp />} />
         <Route path="/summary" element={<Summary />} />
