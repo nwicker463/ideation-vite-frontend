1a503b0d1709e17e0fdd2be6434cbb09ae1a1ef7
Nate Wicker <wickern7@gmail.com>
2025-09-01 12:30:14 -0400

    removing tailwind references from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import { Label } from '@/components/ui/label';
 import './App.css';
 import { Link } from 'react-router-dom';
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+
 
 
 const conjunctivePhrases = [
@@ -19,7 +19,6 @@ const conjunctivePhrases = [
 ];
 
 export default function IdeationGame() {
-  plugins: [tailwindcss()]
   const [ideas, setIdeas] = useState([]);
   const [parentId, setParentId] = useState(null);
   const [content, setContent] = useState('');
