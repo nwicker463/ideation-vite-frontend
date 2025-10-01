94f9f4c11f97a3ecb25ab2dc633c22692ea10862
Nate Wicker <wickern7@gmail.com>
2025-10-01 11:42:19 -0400

    importing useRef

--- src/WaitingRoom.jsx
+++ src/WaitingRoom.jsx
@@ -1,5 +1,5 @@
 import { v4 as uuidv4 } from "uuid";
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 export default function WaitingRoom() {
