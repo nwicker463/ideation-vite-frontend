663c9b145c7426cd0b56826bdc5f043ec66aa0dc
Nate Wicker <wickern7@gmail.com>
2025-11-19 09:33:48 -0500

    removing heartbeat from front end

--- src/WaitingRoom.jsx
+++ src/WaitingRoom.jsx
@@ -64,9 +64,9 @@ export default function WaitingRoom() {
         .catch(err => console.error("Error fetching group ID:", err));
 
 
-      fetch(`${import.meta.env.VITE_API_URL}/api/waiting/${userId}/heartbeat`, {
+      /*fetch(`${import.meta.env.VITE_API_URL}/api/waiting/${userId}/heartbeat`, {
         method: "POST"
-      });
+      });*/
 
 
     }, 2000);
